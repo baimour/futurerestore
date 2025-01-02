2d1a8533da8f8d86daa5d6f1190d8cb9f42aa0c5
baimour <85350961+baimour@users.noreply.github.com>
2025-01-03 00:39:11 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -86,52 +86,52 @@ static struct option longopts[] = {
 bool manual = false;
 
 void cmd_help(){
-    printf("Usage: futurerestore [OPTIONS] iPSW\n");
-    printf("Allows restoring to non-matching firmware with custom SEP+baseband\n");
-    printf("\nGeneral options:\n");
-    printf("  -h, --help\t\t\t\tShows this usage message\n");
-    printf("  -t, --apticket PATH\t\t\tSigning tickets used for restoring\n");
-    printf("  -u, --update\t\t\t\tUpdate instead of erase install (requires appropriate APTicket)\n");
-    printf("              \t\t\t\tDO NOT use this parameter, if you update from jailbroken firmware!\n");
-    printf("  -w, --wait\t\t\t\tKeep rebooting until ApNonce matches APTicket (ApNonce collision, unreliable)\n");
-    printf("  -d, --debug\t\t\t\tShow all code, use to save a log for debug testing\n");
-    printf("  -e, --exit-recovery\t\t\tExit recovery mode and quit\n");
-    printf("  -z, --no-restore\t\t\tDo not restore and end right before NOR data is sent\n");
-    printf("  -c, --custom-latest VERSION\t\tSpecify custom latest version to use for SEP, Baseband and other FirmwareUpdater components\n");
-    printf("  -g, --custom-latest-buildid BUILDID\tSpecify custom latest buildid to use for SEP, Baseband and other FirmwareUpdater components\n");
-    printf("  -i, --custom-latest-beta\t\tGet custom url from list of beta firmwares\n");
-    printf("  -k, --custom-latest-ota\t\tGet custom url from list of ota firmwares");
+    printf("使用方法: futurerestore [选项] IPSW\n");
+    printf("允许使用自定义SEP+基带恢复到不匹配的固件\n");
+    printf("\n常规选项:\n");
+    printf("  -h, --help\t\t\t\t显示使用帮助\n");
+    printf("  -t, --apticket PATH\t\t\t用于恢复的签名票证\n");
+    printf("  -u, --update\t\t\t\t更新而不是抹除安装 (需要适当的APTicket)\n");
+    printf("              \t\t\t\t如果您从已越狱的固件更新，请不要使用此参数！\n");
+    printf("  -w, --wait\t\t\t\t继续重启，直到ApNonce匹配APTicket (ApNonce碰撞，不可靠)\n");
+    printf("  -d, --debug\t\t\t\t显示所有代码，用于保存调试测试日志\n");
+    printf("  -e, --exit-recovery\t\t\t退出恢复模式并退出\n");
+    printf("  -z, --no-restore\t\t\t在发送NOR数据之前不恢复和结束\n");
+    printf("  -c, --custom-latest VERSION\t\t指定用于SEP、基带和其他FirmwareUpdater组件的自定义最新版本\n");
+    printf("  -g, --custom-latest-buildid BUILDID\t指定用于SEP、基带和其他FirmwareUpdater组件的自定义最新Build ID\n");
+    printf("  -i, --custom-latest-beta\t\t从测试版固件列表中获取自定义链接\n");
+    printf("  -k, --custom-latest-ota\t\t从OTA固件列表中获取自定义链接");
 
 #ifdef HAVE_LIBIPATCHER
-    printf("\nOptions for downgrading with Odysseus:\n");
-    printf("  -3, --use-pwndfu\t\t\tRestoring devices with Odysseus method. Device needs to be in pwned DFU mode already\n");
-    printf("  -4, --no-ibss\t\t\t\tRestoring devices with Odysseus method. For checkm8/iPwnder32 specifically, bootrom needs to be patched already with unless iPwnder.\n");
-    printf("  -5, --rdsk PATH\t\t\tSet custom restore ramdisk for entering restoremode(requires use-pwndfu)\n");
-    printf("  -6, --rkrn PATH\t\t\tSet custom restore kernelcache for entering restoremode(requires use-pwndfu)\n");
-    printf("  -7, --set-nonce\t\t\tSet custom nonce from your blob then exit recovery(requires use-pwndfu)\n");
-    printf("  -7, --set-nonce=0xNONCE\t\tSet custom nonce then exit recovery(requires use-pwndfu)\n");
-    printf("  -8, --serial\t\t\t\tEnable serial during boot(requires serial cable and use-pwndfu)\n");
-    printf("  -9, --boot-args\t\t\tSet custom restore boot-args(PROCEED WITH CAUTION)(requires use-pwndfu)\n");
-    printf("  -a, --no-cache\t\t\tDisable cached patched iBSS/iBEC(requires use-pwndfu)\n");
-    printf("  -f, --skip-blob\t\t\tSkip SHSH blob validation(PROCEED WITH CAUTION)(requires use-pwndfu)\n");
+    printf("\nOdysseus降级选项:\n");
+    printf("  -3, --use-pwndfu\t\t\t使用Odysseus方法恢复设备，设备需要已经处于pwed DFU模式\n");
+    printf("  -4, --no-ibss\t\t\t\t使用Odysseus方法恢复设备，特别是对于checkm8/iPwnder32，除非iPwnder已经为引导程序打好的补丁\n");
+    printf("  -5, --rdsk PATH\t\t\t设置自定义还原ramdisk以进入还原模式 (需要 use-pwndfu)\n");
+    printf("  -6, --rkrn PATH\t\t\t设置自定义恢复内核缓存以进入还原模式(需要 use-pwndfu)\n");
+    printf("  -7, --set-nonce\t\t\t从Blob设置自定义Nonce，然后退出恢复(需要 use-pwndfu)\n");
+    printf("  -7, --set-nonce=0xNONCE\t\t设置自定义Nonce，然后退出恢复(需要 use-pwndfu)\n");
+    printf("  -8, --serial\t\t\t\t在引导期间启用串行 (需要串行电缆和use-pwndfu)\n");
+    printf("  -9, --boot-args\t\t\t设置自定义还原启动参数 (谨慎使用，且需要 use-pwndfu)\n");
+    printf("  -a, --no-cache\t\t\t禁用已缓存并已修补的iBSS/iBEC (需要 use-pwndfu)\n");
+    printf("  -f, --skip-blob\t\t\t跳过SHSH Blob验证 (谨慎使用，且需要 use-pwndfu)\n");
 #endif
 
-    printf("\nOptions for SEP:\n");
-    printf("  -0, --latest-sep\t\t\tUse latest signed SEP instead of manually specifying one\n");
+    printf("\nSEP选项:\n");
+    printf("  -0, --latest-sep\t\t\t使用最新签名的SEP而不是手动指定一个\n");
     if(manual) {
-        printf("  -s, --sep PATH\t\t\tSEP to be flashed\n");
-        printf("  -m, --sep-manifest PATH\t\tBuildManifest for requesting SEP ticket\n");
+        printf("  -s, --sep PATH\t\t\tSEP刷新\n");
+        printf("  -m, --sep-manifest PATH\t\t用于请求SEP Ticket的BuildManifest\n");
     }
-    printf("  -j, --no-rsep\t\t\t\tChoose not to send Restore Mode SEP firmware command\n");
+    printf("  -j, --no-rsep\t\t\t\t选择不发送还原模式SEP固件命令\n");
 
-    printf("\nOptions for baseband:\n");
-    printf("  -1, --latest-baseband\t\t\tUse latest signed baseband instead of manually specifying one\n");
+    printf("\n基带选项:\n");
+    printf("  -1, --latest-baseband\t\t\t使用最新签名的基带而不是手动指定一个\n");
     if(manual) {
-        printf("  -b, --baseband PATH\t\t\tBaseband to be flashed\n");
-        printf("  -p, --baseband-manifest PATH\t\tBuildManifest for requesting baseband ticket\n");
+        printf("  -b, --baseband PATH\t\t\t基带刷新\n");
+        printf("  -p, --baseband-manifest PATH\t\t用于请求基带Ticket的BuildManifest\n");
     }
-    printf("  -2, --no-baseband\t\t\tSkip checks and don't flash baseband\n");
-    printf("                   \t\t\tOnly use this for device without a baseband (eg. iPod touch or some Wi-Fi only iPads)\n\n");
+    printf("  -2, --no-baseband\t\t\t跳过检查，不刷新基带\n");
+    printf("                   \t\t\t仅适用于没有基带的设备 (例如iPod Touch或仅支持WiFi的iPad)\n\n");
 }
 
 using namespace std;
@@ -144,14 +144,14 @@ int main_r(int argc, const char * argv[]) {
         SetConsoleMode(handle, termFlags | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
 #endif
     int err=0;
-    printf("Version: " VERSION_RELEASE "(" VERSION_COMMIT_SHA "-" VERSION_COMMIT_COUNT ")\n");
+    printf("版本: " VERSION_RELEASE "(" VERSION_COMMIT_SHA "-" VERSION_COMMIT_COUNT ")\n");
     printf("%s\n",tihmstar::img4tool::version());
 #ifdef HAVE_LIBIPATCHER
     printf("%s\n",libipatcher::version());
-    printf("Odysseus for 32-bit support: yes\n");
-    printf("Odysseus for 64-bit support: %s\n",(libipatcher::has64bitSupport() ? "yes" : "no"));
+    printf("Odysseus支持32位: yes\n");
+    printf("Odysseus支持64位: %s\n",(libipatcher::has64bitSupport() ? "yes" : "no"));
 #else
-    printf("Odysseus support: no\n");
+    printf("Odysseus支持: no\n");
 #endif
 
     int optindex = 0;
@@ -179,7 +179,7 @@ int main_r(int argc, const char * argv[]) {
     char *legacy = std::getenv("FUTURERESTORE_I_SOLEMNLY_SWEAR_THAT_I_AM_UP_TO_NO_GOOD");
     manual = legacy != nullptr;
     if(manual) {
-        info("WARNING: User specified to enable Deprecated/Legacy options, at risk of boot loop!\n");
+        info("警告：用户指定启用了已弃用/旧的选项，有无限重启的风险！\n");
     }
 
     if (argc == 1){
@@ -197,19 +197,19 @@ int main_r(int argc, const char * argv[]) {
                 apticketPaths.push_back(optarg);
                 break;
             case 'b': // long option: "baseband"; can be called as short option
-                retassure(manual, "--baseband is Deprecated! Please switch to --custom-latest or --custom-latest-beta.");
+                retassure(manual, "--baseband 已弃用！请切换到 --custom-latest 或者 --custom-latest-beta");
                 basebandPath = optarg;
                 break;
             case 'p': // long option: "baseband-manifest"; can be called as short option
-                retassure(manual, "--baseband-manifest is Deprecated! Please switch to --custom-latest or --custom-latest-beta.");
+                retassure(manual, "--baseband-manifest 已弃用！请切换到 --custom-latest 或者 --custom-latest-beta.");
                 basebandManifestPath = optarg;
                 break;
             case 's': // long option: "sep"; can be called as short option
-                retassure(manual, "--sep is Deprecated! Please switch to --custom-latest or --custom-latest-beta.");
+                retassure(manual, "--sep 已弃用！请切换到 --custom-latest 或者 --custom-latest-beta.");
                 sepPath = optarg;
                 break;
             case 'm': // long option: "sep-manifest"; can be called as short option
-                retassure(manual, "--sep-manifest is Deprecated! Please switch to --custom-latest or --custom-latest-beta.");
+                retassure(manual, "--sep-manifest 已弃用！请切换到 --custom-latest 或者 --custom-latest-beta.");
                 sepManifestPath = optarg;
                 break;
             case 'w': // long option: "wait"; can be called as short option
@@ -264,9 +264,9 @@ int main_r(int argc, const char * argv[]) {
                 custom_nonce = (optarg) ? optarg : nullptr;
                 if(custom_nonce != nullptr) {
                     uint64_t gen;
-                    retassure(strlen(custom_nonce) == 16 || strlen(custom_nonce) == 18,"Incorrect nonce length!\n");
+                    retassure(strlen(custom_nonce) == 16 || strlen(custom_nonce) == 18,"Nonce长度不正确！\n");
                     gen = std::stoul(custom_nonce, nullptr, 16);
-                    retassure(gen, "failed to parse generator. Make sure it is in format 0x%16llx");
+                    retassure(gen, "解析生成器失败，确保格式为0x%16llx");
                 }
                 break;
             case '8': // long option: "serial";
@@ -303,11 +303,11 @@ int main_r(int argc, const char * argv[]) {
 
         ipsw = argv[0];
     }else if (argc == optind && flags & FLAG_WAIT) {
-        info("User requested to only wait for ApNonce to match, but not for actually restoring\n");
+        info("用户请求只等待ApNonce匹配，而不是实际还原\n");
     }else if (exitRecovery){
-        info("Exiting from recovery mode to normal mode\n");
+        info("正在从恢复模式退出到正常模式\n");
     }else{
-        error("argument parsing failed! agrc=%d optind=%d\n",argc,optind);
+        error("参数解析失败！agrc=%d optind=%d\n",argc,optind);
         if (idevicerestore_debug){
             for (int i=0; i<argc; i++) {
                 printf("argv[%d]=%s\n",i,argv[i]);
@@ -317,37 +317,37 @@ int main_r(int argc, const char * argv[]) {
     }
 
     futurerestore client(flags & FLAG_UPDATE, flags & FLAG_IS_PWN_DFU, flags & FLAG_NO_IBSS, flags & FLAG_SET_NONCE, flags & FLAG_SERIAL, flags & FLAG_NO_RESTORE_FR, flags & FLAG_NO_RSEP_FR);
-    retassure(client.init(),"can't init, no device found\n");
+    retassure(client.init(),"无法初始化，找不到设备\n");
 
-    printf("futurerestore init done\n");
+    printf("futurerestore初始化完成\n");
     if(flags & FLAG_NO_IBSS)
-        retassure((flags & FLAG_IS_PWN_DFU),"--no-ibss requires --use-pwndfu\n");
+        retassure((flags & FLAG_IS_PWN_DFU),"--no-ibss 需要 --use-pwndfu\n");
     if(flags & FLAG_RESTORE_RAMDISK)
-        retassure((flags & FLAG_IS_PWN_DFU),"--rdsk requires --use-pwndfu\n");
+        retassure((flags & FLAG_IS_PWN_DFU),"--rdsk 需要 --use-pwndfu\n");
     if(flags & FLAG_RESTORE_KERNEL)
-        retassure((flags & FLAG_IS_PWN_DFU),"--rkrn requires --use-pwndfu\n");
+        retassure((flags & FLAG_IS_PWN_DFU),"--rkrn 需要 --use-pwndfu\n");
     if(flags & FLAG_SET_NONCE)
-        retassure((flags & FLAG_IS_PWN_DFU),"--set-nonce requires --use-pwndfu\n");
+        retassure((flags & FLAG_IS_PWN_DFU),"--set-nonce 需要 --use-pwndfu\n");
     if(flags & FLAG_SET_NONCE && client.is32bit())
-        error("--set-nonce not supported on 32bit devices.\n");
+        error("--set-nonce 不支持32位设备\n");
     if(flags & FLAG_RESTORE_RAMDISK)
-        retassure((flags & FLAG_RESTORE_KERNEL),"--rdsk requires --rkrn\n");
+        retassure((flags & FLAG_RESTORE_KERNEL),"--rdsk 需要 --rkrn\n");
     if(flags & FLAG_SERIAL) {
-        retassure((flags & FLAG_IS_PWN_DFU),"--serial requires --use-pwndfu\n");
-        retassure(!(flags & FLAG_BOOT_ARGS),"--serial conflicts with --boot-args\n");
+        retassure((flags & FLAG_IS_PWN_DFU),"--serial 需要 --use-pwndfu\n");
+        retassure(!(flags & FLAG_BOOT_ARGS),"--serial 与 --boot-args冲突\n");
     }
     if(flags & FLAG_BOOT_ARGS)
-        retassure((flags & FLAG_IS_PWN_DFU),"--boot-args requires --use-pwndfu\n");
+        retassure((flags & FLAG_IS_PWN_DFU),"--boot-args 需要 --use-pwndfu\n");
     if(flags & FLAG_NO_CACHE)
-        retassure((flags & FLAG_IS_PWN_DFU),"--no-cache requires --use-pwndfu\n");
+        retassure((flags & FLAG_IS_PWN_DFU),"--no-cache 需要 --use-pwndfu\n");
     if(flags & FLAG_SKIP_BLOB)
-        retassure((flags & FLAG_IS_PWN_DFU),"--skip-blob requires --use-pwndfu\n");
+        retassure((flags & FLAG_IS_PWN_DFU),"--skip-blob 需要 --use-pwndfu\n");
     if(flags & FLAG_CUSTOM_LATEST_BETA)
-        retassure((flags & FLAG_CUSTOM_LATEST_BUILDID),"-i, --custom-latest-beta requires -g, --custom-latest-buildid\n");
+        retassure((flags & FLAG_CUSTOM_LATEST_BUILDID),"-i, --custom-latest-beta 需要 -g, --custom-latest-buildid\n");
     if(flags & FLAG_CUSTOM_LATEST_OTA)
-        retassure((flags & FLAG_CUSTOM_LATEST_BUILDID),"-k, --custom-latest-ota requires -g, --custom-latest-buildid\n");
+        retassure((flags & FLAG_CUSTOM_LATEST_BUILDID),"-k, --custom-latest-ota 需要 -g, --custom-latest-buildid\n");
     if(flags & FLAG_CUSTOM_LATEST_BUILDID)
-        retassure((flags & FLAG_CUSTOM_LATEST) == 0,"-g, --custom-latest-buildid is not compatible with -c, --custom-latest\n");
+        retassure((flags & FLAG_CUSTOM_LATEST) == 0,"-g, --custom-latest-buildid 与 -c, --custom-latest不兼容\n");
 
     if (exitRecovery) {
         client.exitRecovery();
@@ -374,7 +374,7 @@ int main_r(int argc, const char * argv[]) {
         )) {
 
             if (!(flags & FLAG_WAIT) || ipsw){
-                error("missing argument\n");
+                error("缺少一些参数！\n");
                 cmd_help();
                 err = -2;
             }else{
@@ -415,7 +415,7 @@ int main_r(int argc, const char * argv[]) {
         }
         if(!(flags & FLAG_SET_NONCE)) {
             if (flags & FLAG_LATEST_SEP) {
-                info("User specified to use latest signed SEP\n");
+                info("用户指定使用最新签名的SEP\n");
                 client.downloadLatestSep();
             } else if (!client.is32bit()) {
                 client.setSepPath(sepPath);
@@ -430,19 +430,19 @@ int main_r(int argc, const char * argv[]) {
 
         versVals.basebandMode = kBasebandModeWithoutBaseband;
         if(!(flags & FLAG_SET_NONCE)) {
-            info("Checking if SEP is being signed...\n");
+            info("正在检查SEP是否处于签名状态...\n");
             if (!client.is32bit() &&
                 !(isManifestSignedForDevice(client.getSepManifestPath().c_str(), &devVals, &versVals, nullptr))) {
-                reterror("SEP firmware is NOT being signed!\n");
+                reterror("SEP固件未处于签名状态！\n");
             } else {
-                info("SEP is being signed!\n");
+                info("SEP处于签名状态！\n");
             }
         }
         if (flags & FLAG_NO_BASEBAND){
-            info("\nWARNING: user specified is not to flash a baseband. This can make the restore fail if the device needs a baseband!\n");
-            info("\nIf you added this flag by mistake, you can press CTRL-C now to cancel\n");
+            info("\n警告：用户指定了是不刷新基带。如果设备需要基带，这可能会使还原失败！\n");
+            info("\n如果您错误地添加了此选项，现在可以按CTRL+C取消\n");
             int c = 10;
-            info("Continuing restore in ");
+            info("正在继续还原 ");
             while (c) {
                 info("%d ",c--);
                 fflush(stdout);
@@ -452,28 +452,28 @@ int main_r(int argc, const char * argv[]) {
         }else{
             if(!(flags & FLAG_SET_NONCE)) {
                 if (flags & FLAG_LATEST_BASEBAND) {
-                    info("User specified to use latest signed baseband\n");
+                    info("用户指定使用最新签名的基带\n");
                     client.downloadLatestBaseband();
                 } else {
                     client.setBasebandPath(basebandPath);
                     client.setBasebandManifestPath(basebandManifestPath);
                     client.loadBaseband(basebandPath);
                     client.loadBasebandManifest(basebandManifestPath);
-                    info("Did set SEP and baseband path and firmware\n");
+                    info("已设置了SEP和基带路径和固件\n");
                 }
             }
 
             versVals.basebandMode = kBasebandModeOnlyBaseband;
             if (!(devVals.bbgcid = client.getBasebandGoldCertIDFromDevice())){
-                debug("[WARNING] using tsschecker's fallback to get BasebandGoldCertID. This might result in invalid baseband signing status information\n");
+                debug("[警告] 正在使用Tsschecker的回退来获取Baseband GoldCertID，这可能会导致基带签名状态信息无效！\n");
             }
             if(!(flags & FLAG_SET_NONCE)) {
-                info("Checking if Baseband is being signed...\n");
+                info("正在检查基带是否处于签名状态...\n");
                 if (!(isManifestSignedForDevice(client.getBasebandManifestPath().c_str(), &devVals, &versVals,
                                                 nullptr))) {
-                    reterror("Baseband firmware is NOT being signed!\n");
+                    reterror("基带固件未处于签名状态！\n");
                 } else {
-                    info("Baseband is being signed!\n");
+                    info("基带处于签名状态！\n");
                 }
             }
         }
@@ -486,21 +486,21 @@ int main_r(int argc, const char * argv[]) {
         }
     } catch (int error) {
         err = error;
-        printf("[Error] Fail code=%d\n",err);
+        printf("[错误] 失败代码=%d\n",err);
         goto error;
     }
 
     try {
         client.doRestore(ipsw);
-        printf("Done: restoring succeeded!\n");
+        printf("执行完成: 还原成功！\n");
     } catch (tihmstar::exception &e) {
         e.dump();
-        printf("Done: restoring failed!\n");
+        printf("执行完成: 还原失败！\n");
     }
 
     error:
     if (err){
-        printf("Failed with error code=%d\n",err);
+        printf("失败，错误代码为=%d\n",err);
     }
     return err;
 #undef reterror
@@ -513,7 +513,7 @@ int main(int argc, const char * argv[]) {
     try {
         return main_r(argc, argv);
     } catch (tihmstar::exception &e) {
-        printf("%s: failed with exception:\n",PACKAGE_NAME);
+        printf("%s: 失败，出现异常:\n",PACKAGE_NAME);
         e.dump();
         return e.code();
     }
